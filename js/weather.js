bbf6e1f97372ac0fe6cfc5ba38be1285251159c4
lmingi <mingilee98@gmail.com>
2022-04-10 23:36:11 -0500

    add weather API

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,19 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "b0f9c8f59077e6bb8f9254fa713be9d1";
+
+
 
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("lat:", lat, "log:" + lng);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });
 }
 function onGeoError() {
     alert("Please allow location access.");
